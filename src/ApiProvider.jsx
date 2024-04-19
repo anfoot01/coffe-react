5d2068041d12ff00877bad68d02732f4730aa87f
Anton Zinoviev <avz010308@gmail.com>
2024-04-19 21:27:08 +0200

    fixing navigation

--- src/ApiProvider.jsx
+++ src/ApiProvider.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { createContext, useMemo } from 'react';
 import { Header } from './shared/components/Header/Header';
 import { Sidebar } from './shared/components/Sidebar/Sidebar';
@@ -6,15 +6,18 @@ import { Sidebar } from './shared/components/Sidebar/Sidebar';
 export const ApiContext = createContext({});
 
 export const ApiProvider = () => {
+    const location = useLocation();
+    const currentPath = location.pathname;    
+    console.log(currentPath);
     const value = useMemo(() => ({}), []);
 
     return (
         <ApiContext.Provider value={value}>
-            <Header />
+            {currentPath !== "/product" && <Header />}
             <div className="main">
                 <Outlet />
             </div>
-            <Sidebar />
+            {currentPath !== "/product" && <Sidebar />}
         </ApiContext.Provider>
     );
 };
